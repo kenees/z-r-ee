ed7406ad30ca20554c13cce8662d4ab85c1b7deb
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2025-02-20 11:37:08 +0100

    chore: update repack

--- apps/mobile-inventory/rspack.config.mjs
+++ apps/mobile-inventory/rspack.config.mjs
@@ -1,19 +1,16 @@
-import {createRequire} from 'node:module';
 import * as Repack from '@callstack/repack';
+import {RsdoctorRspackPlugin} from '@rsdoctor/rspack-plugin';
 import rspack from '@rspack/core';
 import {getSharedDependencies} from 'mobile-sdk';
 import path from 'node:path';
+import {fileURLToPath} from 'node:url';
 import {withZephyr} from 'zephyr-repack-plugin';
 
-/**
- * This env variable shows if bundle is standalone and eager should be enabled in Module federation Plugin config.
- * Please see more detailed description in Module Federation Plugin config.
- */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const STANDALONE = Boolean(process.env.STANDALONE);
 const USE_ZEPHYR = Boolean(process.env.ZC);
-
-const dirname = Repack.getDirname(import.meta.url);
-const {resolve} = createRequire(import.meta.url);
 
 /**
  * More documentation, installation, usage, motivation and differences with Metro is available at:
@@ -31,193 +28,59 @@ const {resolve} = createRequire(import.meta.url);
  *            when running with `react-native start/bundle`.
  */
 export default env => {
-  const {
-    mode = 'development',
-    context = dirname,
-    entry = './index.js',
-    platform = process.env.PLATFORM,
-    minimize = mode === 'production',
-    devServer = undefined,
-    bundleFilename = undefined,
-    sourceMapFilename = undefined,
-    assetsPath = undefined,
-    reactNativePath = resolve('react-native'),
-  } = env;
-
-  if (!platform) {
-    throw new Error('Missing platform');
-  }
+  const {mode, platform = process.env.PLATFORM} = env;
 
   const config = {
     mode,
-    /**
-     * This should be always `false`, since the Source Map configuration is done
-     * by `SourceMapDevToolPlugin`.
-     */
-    devtool: false,
-    context,
-    // empty entry when building as federated miniapp
-    // TODO remove this workaround when fixed in Re.Pack
-    entry: STANDALONE ? entry : {},
+    context: __dirname,
+    entry: './index.js',
+    experiments: {
+      incremental: mode === 'development',
+    },
     resolve: {
-      /**
-       * `getResolveOptions` returns additional resolution configuration for React Native.
-       * If it's removed, you won't be able to use `<file>.<platform>.<ext>` (eg: `file.ios.js`)
-       * convention and some 3rd-party libraries that specify `react-native` field
-       * in their `package.json` might not work correctly.
-       */
-      ...Repack.getResolveOptions(platform),
-
-      /**
-       * Uncomment this to ensure all `react-native*` imports will resolve to the same React Native
-       * dependency. You might need it when using workspaces/monorepos or unconventional project
-       * structure. For simple/typical project you won't need it.
-       */
-      // alias: {
-      //   'react-native': reactNativePath,
-      // },
+      ...Repack.getResolveOptions(),
     },
-    /**
-     * Configures output.
-     * It's recommended to leave it as it is unless you know what you're doing.
-     * By default Webpack will emit files into the directory specified under `path`. In order for the
-     * React Native app use them when bundling the `.ipa`/`.apk`, they need to be copied over with
-     * `Repack.OutputPlugin`, which is configured by default inside `Repack.RepackPlugin`.
-     */
     output: {
-      clean: true,
-      hashFunction: 'xxhash64',
-      path: path.join(dirname, 'build/generated', platform),
-      filename: 'index.bundle',
-      chunkFilename: '[name].chunk.bundle',
-      publicPath: Repack.getPublicPath({platform, devServer}),
       uniqueName: 'mobile-inventory',
-    },
-    /** Configures optimization of the built bundle. */
-    optimization: {
-      /** Enables minification based on values passed from React Native Community CLI or from fallback. */
-      minimize,
-      /** Configure minimizer to process the bundle. */
-      chunkIds: 'named',
     },
     module: {
       rules: [
-        Repack.REACT_NATIVE_LOADING_RULES,
-        Repack.NODE_MODULES_LOADING_RULES,
-        Repack.FLOW_TYPED_MODULES_LOADING_RULES,
-        /** Here you can adjust loader that will process your files. */
-        {
-          test: /\.[jt]sx?$/,
-          exclude: [/node_modules/],
-          type: 'javascript/auto',
-          use: {
-            loader: 'builtin:swc-loader',
-            /** @type {import('@rspack/core').SwcLoaderOptions} */
-            options: {
-              env: {
-                targets: {
-                  'react-native': '0.74',
-                },
-              },
-              assumptions: {
-                setPublicClassFields: true,
-                privateFieldsAsProperties: true,
-              },
-              jsc: {
-                externalHelpers: true,
-                transform: {
-                  react: {
-                    runtime: 'automatic',
-                    development: mode === 'development',
-                    refresh: mode === 'development' && Boolean(devServer),
-                  },
-                },
-              },
-            },
-          },
-        },
-        /** Run React Native codegen, required for utilizing new architecture */
-        Repack.REACT_NATIVE_CODEGEN_RULES,
-        /**
-         * This loader handles all static assets (images, video, audio and others), so that you can
-         * use (reference) them inside your application.
-         *
-         * If you want to handle specific asset type manually, filter out the extension
-         * from `ASSET_EXTENSIONS`, for example:
-         * ```
-         * Repack.ASSET_EXTENSIONS.filter((ext) => ext !== 'svg')
-         * ```
-         */
-        {
-          test: Repack.getAssetExtensionsRegExp([
-            'lottie',
-            ...Repack.ASSET_EXTENSIONS,
-          ]),
-          use: {
-            loader: '@callstack/repack/assets-loader',
-            options: {
-              platform,
-              devServerEnabled: Boolean(devServer),
-              inline: true,
-            },
-          },
-        },
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules({inline: !STANDALONE}),
       ],
     },
     plugins: [
-      /**
-       * Configure other required and additional plugins to make the bundle
-       * work in React Native and provide good development experience with
-       * sensible defaults.
-       *
-       * `Repack.RepackPlugin` provides some degree of customization, but if you
-       * need more control, you can replace `Repack.RepackPlugin` with plugins
-       * from `Repack.plugins`.
-       */
-      new Repack.RepackPlugin({
-        context,
-        mode,
-        platform,
-        devServer,
-        output: {
-          bundleFilename,
-          sourceMapFilename,
-          assetsPath,
-        },
-      }),
-      /**
-       * This plugin is nessessary to make Module Federation work.
-       */
+      new Repack.RepackPlugin(),
       new Repack.plugins.ModuleFederationPluginV2({
-        /**
-         * The name of the module is used to identify the module in URLs resolver and imports.
-         */
         name: 'MobileInventory',
         filename: 'MobileInventory.container.js.bundle',
         dts: false,
         remotes: {
           MobileCart: `MobileCart@http://localhost:9000/${platform}/MobileCart.container.js.bundle`,
         },
-        /**
-         * This is a list of modules that will be shared between remote containers.
-         */
-        exposes: {
-          './HomeScreen': './src/screens/HomeScreen',
-          './ProductDetailsScreen': './src/screens/ProductDetailsScreen',
-        },
-        /**
-         * Shared modules are shared in the share scope.
-         * React, React Native and React Navigation should be provided here because there should be only one instance of these modules.
-         * Their names are used to match requested modules in this compilation.
-         */
+        exposes: STANDALONE
+          ? undefined
+          : {
+              './HomeScreen': './src/screens/HomeScreen',
+              './ProductDetailsScreen': './src/screens/ProductDetailsScreen',
+            },
         shared: getSharedDependencies({eager: STANDALONE}),
       }),
-      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
       new rspack.IgnorePlugin({
         resourceRegExp: /^@react-native-masked-view/,
       }),
     ],
   };
+
+  if (process.env.RSDOCTOR) {
+    config.plugins.push(
+      new RsdoctorRspackPlugin({
+        supports: {
+          generateTileGraph: true,
+        },
+      }),
+    );
+  }
 
   if (USE_ZEPHYR) {
     return withZephyr()(config);
